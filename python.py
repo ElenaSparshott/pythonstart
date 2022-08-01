abaf9bcfa56c1a5ae0a6ff91f61d8e831d7c9853
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-08-01 23:13:41 +0100

    in range

--- python.py
+++ python.py
@@ -1,7 +1,24 @@
+from numpy import prod
+
 x = max(5, 8, 9, 10, 50)
 print(x)
+
 
 
 l = sum([10, 20, 30, 40, 50])
 print(l)
 
+
+
+m = prod([4, 8])
+print(m)
+
+
+
+range_1 = range(0, 20, 1)
+number = int(input('Enter a number : '))
+ 
+if number in range_1 :
+    print(number, 'is present in the range.')
+else :
+    print(number, 'is not present in the range.')
